65664a5fe2bb352f9ef25c257d3b52a90767f48e
cecilia sosa <csosa@elevation.com.ar>
2026-01-29 11:26:22 -0300

    replace use-cases by user services, import SharedModule, export userModule

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { UserRepository } from './domain/user.repository';
-import { UserRepositoryImpl } from './infrastructure/user.repository.impl';
-import { GetAllUsersUseCase } from './application/use-cases/get-all-users.use-case';
-import { CreateUserUseCase } from './application/use-cases/create-user.use-case';
-import { UpdateUserUseCase } from './application/use-cases/update-user.use-case';
-import { DeleteUserUseCase } from './application/use-cases/delete-user.use-case';
-import { GetUserByIdWithPokemonUseCase } from './application/use-cases/get-by-id-user-with-pokemon.use-case';
 import { ClientsModule } from 'src/clients/clients.module';
-import { UpdatePokemonIdsUseCase } from './application/use-cases/update-pokemon-ids.use-case';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './domain/entity/user.entity';
+import { User } from './user.entity';
+import { UserRepository } from './infrastructure/user.repository';
+import { GetAllUsersService } from './application/get-all-users.service';
+import { CreateUserService } from './application/create-user.service';
+import { UpdateUserService } from './application/update-user.service';
+import { DeleteUserService } from './application/delete-user.service';
+import { SharedModule } from 'src/shared/shared.module';
+import { getUserByIdService } from './application/get-by-id-user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ClientsModule],
+  imports: [TypeOrmModule.forFeature([User]), ClientsModule, SharedModule],
   controllers: [UserController],
   providers: [
-    { provide: UserRepository, useClass: UserRepositoryImpl },
-    GetAllUsersUseCase,
-    GetUserByIdWithPokemonUseCase,
-    CreateUserUseCase,
-    UpdateUserUseCase,
-    DeleteUserUseCase,
-    UpdatePokemonIdsUseCase,
+    UserRepository,
+    GetAllUsersService,
+    getUserByIdService,
+    CreateUserService,
+    UpdateUserService,
+    DeleteUserService,
   ],
+  exports: [UserRepository],
 })
 export class UserModule {}
 
