a731adfcea1d92ce9d79c01d5e9bec6d51ac3985
cecilia sosa <csosa@elevation.com.ar>
2026-01-29 17:47:54 -0300

    add notification repository

---
+++ src/modules/notifications/infrastructure/notification.repository.ts
@@ -0,0 +1,38 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Notification } from '../notification.entity';
+import { Repository } from 'typeorm';
+
+export class NotificationRepository {
+  constructor(
+    @InjectRepository(Notification)
+    private notificationRepository: Repository<Notification>,
+  ) {}
+
+  findById(id: number): Promise<Notification | null> {
+    return this.notificationRepository.findOneBy({ id });
+  }
+
+  findByIdUser(id: number): Promise<Notification[]> {
+    return this.notificationRepository.find({ where: { user: { id: id } } });
+  }
+
+  async update(
+    id: number,
+    notificationData: Partial<Notification>,
+  ): Promise<Notification | null> {
+    const result = await this.notificationRepository.update(
+      id,
+      notificationData,
+    );
+
+    if (result.affected === 0) return null;
+
+    return await this.findById(id);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const result = await this.notificationRepository.delete(id);
+
+    return result.affected !== 0; //!==0 es true, borro el registro
+  }
+}
