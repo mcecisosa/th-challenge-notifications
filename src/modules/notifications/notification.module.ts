6f250bde874f10fda7f79ede2c346b3a75d73cfd
cecilia sosa <csosa@elevation.com.ar>
2026-01-31 16:01:51 -0300

    add createNotificationService to notification module

--- src/modules/notifications/notification.module.ts
+++ src/modules/notifications/notification.module.ts
@@ -6,12 +6,14 @@ import { NotificationRepository } from './infrastructure/notification.repository
 import { DeleteNotificationService } from './application/delete-notification.service';
 import { UpdateNotificationService } from './application/update-notification.service';
 import { GetNotificationByUserIdService } from './application/get-notification-by-user.service';
+import { CreateNotificationService } from './application/create-notification.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification])],
   controllers: [NotificationController],
   providers: [
     NotificationRepository,
+    CreateNotificationService,
     GetNotificationByUserIdService,
     UpdateNotificationService,
     DeleteNotificationService,
