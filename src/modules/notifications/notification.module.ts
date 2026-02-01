09fb6d693876cc51685ef01f13a03ea90b1cea41
cecilia sosa <csosa@elevation.com.ar>
2026-02-01 17:33:16 -0300

    add delivery repository and strategies to providers in notification module

--- src/modules/notifications/notification.module.ts
+++ src/modules/notifications/notification.module.ts
@@ -7,13 +7,24 @@ import { DeleteNotificationService } from './application/delete-notification.ser
 import { UpdateNotificationService } from './application/update-notification.service';
 import { GetNotificationByUserIdService } from './application/get-notification-by-user.service';
 import { CreateNotificationService } from './application/create-notification.service';
+import { ChannelStrategyResolver } from './infrastructure/strategy-resolver';
+import { EmailStrategy } from './infrastructure/email.channel';
+import { SmsStrategy } from './infrastructure/sms.channel';
+import { PushStrategy } from './infrastructure/push.channel';
+import { DeliveryRepository } from './infrastructure/delivery.repository';
+import { Delivery } from './delivery.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification, Delivery])],
   controllers: [NotificationController],
   providers: [
     NotificationRepository,
+    DeliveryRepository,
     CreateNotificationService,
+    ChannelStrategyResolver,
+    EmailStrategy,
+    SmsStrategy,
+    PushStrategy,
     GetNotificationByUserIdService,
     UpdateNotificationService,
     DeleteNotificationService,
