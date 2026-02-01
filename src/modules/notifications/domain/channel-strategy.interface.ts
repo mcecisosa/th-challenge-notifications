02c0de4287e156850dbe8569688fd9a5eecbe073
cecilia sosa <csosa@elevation.com.ar>
2026-02-01 17:27:31 -0300

    add definition of method validate and send in strategy interface

--- src/modules/notifications/domain/channel-strategy.interface.ts
+++ src/modules/notifications/domain/channel-strategy.interface.ts
@@ -1,3 +1,6 @@
-export interface ChannelStrategy {
-  send(): string;
+import { Notification } from '../notification.entity';
+
+export interface ChannelStrategy<TPayload> {
+  validate(payload: unknown): TPayload;
+  send(payload: TPayload, notification: Notification): Promise<void>;
 }
