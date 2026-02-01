eef8147db12b3b5aef261b736f25827464eb79da
cecilia sosa <csosa@elevation.com.ar>
2026-02-01 17:32:27 -0300

    add delivery repository with create method

---
+++ src/modules/notifications/infrastructure/delivery.repository.ts
@@ -0,0 +1,15 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Delivery } from '../delivery.entity';
+
+export class DeliveryRepository {
+  constructor(
+    @InjectRepository(Delivery)
+    private deliveryRepository: Repository<Delivery>,
+  ) {}
+
+  create(deliveryData: Omit<Delivery, 'id'>): Promise<Delivery> {
+    const delivery = this.deliveryRepository.create(deliveryData);
+    return this.deliveryRepository.save(delivery);
+  }
+}
