ad82df5f1f67339b6185829bb7194dca08ced14c
cecilia sosa <csosa@elevation.com.ar>
2026-02-01 17:34:09 -0300

    refactor to incorporate strategy selector and send notification

--- src/modules/notifications/application/create-notification.service.ts
+++ src/modules/notifications/application/create-notification.service.ts
@@ -1,55 +1,32 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { NotificationRepository } from '../infrastructure/notification.repository';
 import { Notification } from '../notification.entity';
 import { CreateNotificationInput } from '../domain/types/create-notification-input.type';
 import { User } from 'src/modules/users/user.entity';
-import { ChannelTypes } from '../domain/enums/channel.enum';
-import { EmailPayloadDto } from '../dto/email-payload.dto';
-import { plainToInstance } from 'class-transformer';
-import { SmsPayloadDto } from '../dto/sms-payload.dto';
-import { PushPayloadDto } from '../dto/push-payload.dto';
-import { validateSync } from 'class-validator';
+import { ChannelStrategyResolver } from '../infrastructure/strategy-resolver';
 
 @Injectable()
 export class CreateNotificationService {
   constructor(
     private readonly notificationRepository: NotificationRepository,
+    private readonly strategyResolver: ChannelStrategyResolver,
   ) {}
 
   async execute(input: CreateNotificationInput): Promise<Notification> {
-    this.validateByChannel(input.channel, input.payload);
-    //-------
-    const newNotification = this.notificationRepository.create({
+    const strategy = this.strategyResolver.resolve(input.channel);
+
+    const validatePayload = strategy.validate(input.payload);
+
+    const newNotification = await this.notificationRepository.create({
       title: input.title,
       content: input.content,
       channel: input.channel,
       user: { id: input.currUserId } as User,
     });
 
-    // luego → strategy.send(...)
+    await strategy.send(validatePayload, newNotification);
 
     return newNotification;
-  }
-
-  private validateByChannel(channel: ChannelTypes, payload: unknown) {
-    let dto;
-
-    switch (channel) {
-      case ChannelTypes.EMAIL:
-        dto = plainToInstance(EmailPayloadDto, payload);
-        break;
-
-      case ChannelTypes.SMS:
-        dto = plainToInstance(SmsPayloadDto, payload);
-        break;
-
-      case ChannelTypes.PUSH:
-        dto = plainToInstance(PushPayloadDto, payload);
-        break;
-    }
-
-    const errors = validateSync(dto);
-    if (errors.length > 0) throw new BadRequestException(errors);
   }
 }
 
