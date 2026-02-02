c3d733c35ab5c64b3b8ad5f75449e051da77846f
cecilia sosa <csosa@elevation.com.ar>
2026-02-02 17:01:50 -0300

    add clients to providers

--- src/clients/providers.ts
+++ src/clients/providers.ts
@@ -1,4 +1,10 @@
 import { Provider } from '@nestjs/common';
-import { PokemonClient } from './pokemon.client';
+import { EmailClient } from './mail.client';
+import { SmsClient } from './sms.client';
+import { PushClient } from './push.client';
 
-export const ClientsProviders: Provider[] = [PokemonClient];
+export const ClientsProviders: Provider[] = [
+  EmailClient,
+  SmsClient,
+  PushClient,
+];
