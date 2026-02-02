dce442dca88310fd86d6305937e3cb6494313cc8
cecilia sosa <csosa@elevation.com.ar>
2026-02-02 17:04:14 -0300

    refactor send method to call smsClient and manage delivery status

--- src/modules/notifications/infrastructure/sms.channel.ts
+++ src/modules/notifications/infrastructure/sms.channel.ts
@@ -7,10 +7,15 @@ import { SmsPayloadDto } from '../dto/sms-payload.dto';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ChannelTypes } from '../domain/enums/channel.enum';
 import { Notification } from '../notification.entity';
+import { SmsClient } from 'src/clients/sms.client';
+import { DeliveryStatus } from '../domain/enums/delivery.enum';
 
 @Injectable()
 export class SmsStrategy implements ChannelStrategy<SmsPayload> {
-  constructor(private readonly deliveryRepository: DeliveryRepository) {}
+  constructor(
+    private readonly deliveryRepository: DeliveryRepository,
+    private readonly smsClient: SmsClient,
+  ) {}
 
   validate(payload: unknown): SmsPayload {
     const dto = plainToInstance(SmsPayloadDto, payload);
@@ -18,22 +23,33 @@ export class SmsStrategy implements ChannelStrategy<SmsPayload> {
 
     if (errors.length > 0) throw new BadRequestException(errors);
 
-    // if (dto.message.length > 160)
-    //   throw new BadRequestException('SMS exceeds 160 characters');
-
     return dto;
   }
 
   async send(payload: SmsPayload, notification: Notification): Promise<void> {
-    //simular envio
-    console.log(`Enviando sms a ${payload.phoneNumber}`);
-
-    await this.deliveryRepository.create({
+    const newDelivery = await this.deliveryRepository.create({
       notification,
       channel: ChannelTypes.SMS,
-      status: 'SENT',
+      status: DeliveryStatus.PENDING,
       metadata: { phoneNumber: payload.phoneNumber, sentAt: new Date() },
       createdAt: new Date(),
+    });
+
+    let status: string;
+    try {
+      const response = await this.smsClient.sendSms(
+        payload.phoneNumber,
+        notification.content,
+      );
+      status = DeliveryStatus.SENT;
+      console.log('response of emailClient:', response);
+    } catch (error) {
+      console.log(error);
+      status = DeliveryStatus.FAILED;
+    }
+
+    await this.deliveryRepository.update(newDelivery.id, {
+      status: status,
     });
   }
 }
