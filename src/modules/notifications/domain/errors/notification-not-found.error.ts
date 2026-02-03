1157db774f21a06cc369e259012b9dfb0dd2876b
cecilia sosa <csosa@elevation.com.ar>
2026-02-03 17:32:59 -0300

    delete notification and user not found error

--- src/modules/notifications/domain/errors/notification-not-found.error.ts
+++
@@ -1,6 +0,0 @@
-export class NotificationNotFoundError extends Error {
-  constructor(id: number) {
-    super(`Notification with id ${id} not found`);
-    this.name = 'NotificationNotFoundError';
-  }
-}
--- src/modules/users/domain/errors/user-not-found.error.ts
+++
@@ -1,9 +0,0 @@
-export class UserNotFoundError extends Error {
-  constructor(id: number) {
-    super(`User with id ${id} not found`);
-    this.name = 'UserNotFoundError';
-  }
-}
-
-// Esto NO sabe nada de HTTP ni Nest
-// Es dominio puro
