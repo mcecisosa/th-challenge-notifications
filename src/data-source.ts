a2a138f07244dfe029856b28127e4659d52e86fe
cecilia sosa <csosa@elevation.com.ar>
2026-02-10 17:29:00 -0300

    refactor connecction to DB with database_url

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,34 +10,40 @@ dotenv.config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
-// Get environment variables with fallbacks
-// const host = process.env.DATABASE_HOST || 'localhost';
-// const port = parseInt(process.env.DATABASE_PORT || '5432');
-// const username = process.env.DATABASE_USER || 'postgres';
-// const password = process.env.DATABASE_PASSWORD || 'postgres';
-// const database = process.env.DATABASE_NAME || 'app_dev';
+//Get environment variables with fallbacks
+const url = process.env.DATABASE_URL;
+const host = process.env.DATABASE_HOST || 'localhost';
+const port = parseInt(process.env.DATABASE_PORT || '5432');
+const username = process.env.DATABASE_USER || 'postgres';
+const password = process.env.DATABASE_PASSWORD || 'postgres';
+const database = process.env.DATABASE_NAME || 'app_dev';
 //const synchronize = process.env.DATABASE_SYNC === 'true' ? true : false;
 
-const dataSourceConfig = {
-  type: 'postgres' as const,
-  url: process.env.DATABASE_URL,
-  // host,
-  // port,
-  // username,
-  // password,
-  // database,
-  entities: [User, Notification, Delivery],
-  synchronize: true,
-  migrations: [
-    join(__dirname, '..', 'migrations', '*.{ts,js}'),
-    join(__dirname, '..', 'seed-migrations', '*.{ts,js}'),
-  ],
-  migrationsTableName: 'custom_migration_table',
-};
+const dataSourceConfig = url
+  ? {
+      type: 'postgres' as const,
+      url,
+      entities: [User, Notification, Delivery],
+      synchronize: true,
+      migrations: [join(__dirname, '..', 'migrations', '*.{ts,js}')],
+      migrationsTableName: 'custom_migration_table',
+    }
+  : {
+      type: 'postgres' as const,
+      host,
+      port,
+      username,
+      password,
+      database,
+      entities: [User, Notification, Delivery],
+      synchronize: true,
+      migrations: [
+        join(__dirname, '..', 'migrations', '*.{ts,js}'),
+        join(__dirname, '..', 'seed-migrations', '*.{ts,js}'),
+      ],
+      migrationsTableName: 'custom_migration_table',
+    };
 
-console.log(
-  'DB CONFIG:',
-  process.env.DATABASE_URL ? 'DATABASE_URL OK' : 'DATABASE_URL MISSING',
-);
+console.log('DB CONFIG MODE:', url ? 'DATABASE_URL' : 'HOST CONFIG');
 
 export const AppDataSource = new DataSource(dataSourceConfig);
